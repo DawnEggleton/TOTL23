1df09d6c206fec85960c0d80ec2f93bc5a85ca5d
DawnEggleton <dawn.eggleton@carleton.ca>
2023-03-18 10:22:01 -0300

    finalize profile warning, clean scripts

--- dist/js/functions.js
+++ dist/js/functions.js
@@ -1,3 +1,50 @@
+//Settings
+function setTheme() {
+    if(localStorage.getItem('theme') !== null) {
+        switch(localStorage.getItem('theme')) {
+            case 'dark':
+                document.querySelector('body').classList.remove('light');
+                document.querySelector('body').classList.add('dark');
+                break;
+            case 'light':
+            default:
+                document.querySelector('body').classList.add('light');
+                document.querySelector('body').classList.remove('dark');
+                break;
+        }
+    } else {
+        if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+            document.querySelector('body').classList.remove('light');
+            document.querySelector('body').classList.add('dark');
+            localStorage.setItem('theme', 'dark');
+        } else {
+            document.querySelector('body').classList.add('light');
+            document.querySelector('body').classList.remove('dark');
+            localStorage.setItem('theme', 'light');
+        }
+    }
+}
+function setSize() {
+    if(localStorage.getItem('size') !== null) {
+        switch(localStorage.getItem('size')) {
+            case 'large':
+                document.querySelector('body').classList.remove('smFont');
+                document.querySelector('body').classList.add('lgFont');
+                break;
+            case 'small':
+            default:
+                document.querySelector('body').classList.remove('lgFont');
+                document.querySelector('body').classList.add('smFont');
+                break;
+        }
+    } else {
+        document.querySelector('body').classList.remove('lgFont');
+        document.querySelector('body').classList.add('smFont');
+        localStorage.setItem('size', 'small');
+    }
+}
+
+//Toggles
 function toggleMenu() {
     document.querySelector('.nav--menu-icon').classList.toggle('is-open');
     document.querySelector('.nav--main-toggle').classList.toggle('is-open');
@@ -46,50 +93,6 @@ function toggleClose(e) {
         window.removeEventListener('click', toggleClose);
     }
 }
-function setTheme() {
-    if(localStorage.getItem('theme') !== null) {
-        switch(localStorage.getItem('theme')) {
-            case 'dark':
-                document.querySelector('body').classList.remove('light');
-                document.querySelector('body').classList.add('dark');
-                break;
-            case 'light':
-            default:
-                document.querySelector('body').classList.add('light');
-                document.querySelector('body').classList.remove('dark');
-                break;
-        }
-    } else {
-        if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-            document.querySelector('body').classList.remove('light');
-            document.querySelector('body').classList.add('dark');
-            localStorage.setItem('theme', 'dark');
-        } else {
-            document.querySelector('body').classList.add('light');
-            document.querySelector('body').classList.remove('dark');
-            localStorage.setItem('theme', 'light');
-        }
-    }
-}
-function setSize() {
-    if(localStorage.getItem('size') !== null) {
-        switch(localStorage.getItem('size')) {
-            case 'large':
-                document.querySelector('body').classList.remove('smFont');
-                document.querySelector('body').classList.add('lgFont');
-                break;
-            case 'small':
-            default:
-                document.querySelector('body').classList.remove('lgFont');
-                document.querySelector('body').classList.add('smFont');
-                break;
-        }
-    } else {
-        document.querySelector('body').classList.remove('lgFont');
-        document.querySelector('body').classList.add('smFont');
-        localStorage.setItem('size', 'small');
-    }
-}
 function toggleTheme() {
     if(localStorage.getItem('theme') === 'light') {
         localStorage.setItem('theme', 'dark');
@@ -109,6 +112,79 @@ function toggleSize() {
     }
 }
 
+//Initialization
+function initClipboard() {
+    let clipboard = new Clipboard('.clipboard');
+    clipboard.on('success', function(e) {
+        console.log('clipboard success: ' + e);
+    });
+    clipboard.on('error', function(e) {
+        console.log('clipboard error: ' + e);
+    });
+    let clipcode = new Clipboard('.codeclick', {
+        target: function(trigger) {
+        return trigger.nextElementSibling;
+        }
+    });
+}
+function initCodebox() {
+    $("table[id='CODE-WRAP']").each(function() {
+        var cc = $(this).find("td[id='CODE']").html();
+
+        $(this).html(
+            "<div class='codeblock code--wrapper'><div class='c-title codeclick'>Copy</div><div class='codecon'><pre><code class='scroll'>"
+            + cc +
+            "</code></pre></div></div>"
+        );
+    });
+}
+function initSideProfile() {
+    document.querySelectorAll('.popout--name').forEach(breakTitle => {
+        breakTitle.innerHTML = breakTitle.innerText.replace(` `, `<br>`);
+    });
+}
+function initTabs() {
+    document.querySelectorAll('tag-labels').forEach(labelset => {
+        labelset.querySelectorAll('tag-label').forEach((label, i) => {
+            label.addEventListener('click', e => {
+                let labels = label.parentNode.querySelectorAll('tag-label');
+                let tabs = label.parentNode.parentNode.parentNode.querySelectorAll(':scope > tag-tabs > tag-tab');
+                labels.forEach(sibling => sibling.classList.remove('is-active'));
+                tabs.forEach(tab => {
+                    tab.classList.remove('is-active');
+                    tab.style.left = `${-100 * i}%`;
+                });
+                label.classList.add('is-active');
+                tabs[i].classList.add('is-active');
+        
+                if(label.dataset.tab) {
+                    window.location.hash = label.dataset.tab;
+                }
+            });
+        });
+    });
+}
+function initHashTabs() {
+    let hash = window.location.hash.split('#')[1];
+    let label = document.querySelector(`tag-label[data-tab="${hash}"]`);
+    let tab = document.querySelector(`tag-tab[data-tab="${hash}"]`);
+    let labels = label.parentNode.querySelectorAll('tag-label');
+    let tabs = label.parentNode.parentNode.parentNode.querySelectorAll(':scope > tag-tabs > tag-tab');
+    let tabsArray = Array.prototype.slice.call(tabs);
+    let index = tabsArray.indexOf(tab);
+
+    labels.forEach(sibling => sibling.classList.remove('is-active'));
+    tabs.forEach(tab => {
+        tab.classList.remove('is-active');
+        tab.style.left = `${-100 * index}%`;
+    });
+    label.classList.add('is-active');
+    tab.classList.add('is-active');
+
+    window.location.hash = label.dataset.tab;
+}
+
+//Global
 function highlightCode() {
     let clipcodeQuick = new Clipboard('.copyQuick', {
         target: function(trigger) {
@@ -120,10 +196,18 @@ function highlightCode() {
         }
     });
 }
-
 function moveLeft(e) {
     e.parentNode.querySelector('tag-labels').scrollLeft -= 150;
 }
 function moveRight(e) {
     e.parentNode.querySelector('tag-labels').scrollLeft += 150;
+}
+
+//Profile Scripts
+function clipWarnings() {
+    let warnings = `verbal and physical abuse, depression, suicidal attempt/ideation, severe injury`;
+    if(warnings !== `<i>No Information</i>`) {
+        document.querySelector('#clip-warning').innerHTML = `<i class="fa-regular fa-triangle-exclamation"></i>
+        <span>${warnings}</span>`;
+    }
 }
--- source/js/scripts.js
+++ source/js/scripts.js
@@ -1,71 +1,33 @@
-//run start up functions - keep first
+//set variables
+let pageID = document.querySelector('body').id;
+
+//init global
 setTheme();
 setSize();
+initSideProfile();
 
-//copy code to clipboard
-let clipboard = new Clipboard('.clipboard');
-clipboard.on('success', function(e) {
-    console.log('clipboard success: ' + e);
-});
-clipboard.on('error', function(e) {
-    console.log('clipboard error: ' + e);
-});
-let clipcode = new Clipboard('.codeclick', {
-    target: function(trigger) {
-    return trigger.nextElementSibling;
-    }
-});
-$("table[id='CODE-WRAP']").each(function() {
-    var cc = $(this).find("td[id='CODE']").html();
-
-    $(this).html(
-        "<div class='codeblock code--wrapper'><div class='c-title codeclick'>Copy</div><div class='codecon'><pre><code class='scroll'>"
-        + cc +
-        "</code></pre></div></div>"
-    );
-});
-
-//profile popout linebreaks
-document.querySelectorAll('.popout--name').forEach(breakTitle => {
-    breakTitle.innerHTML = breakTitle.innerText.replace(` `, `<br>`);
-});
-
-//tabs
-if(window.location.hash && document.querySelectorAll('tag-label').length !== 0) {
-    let hash = window.location.hash.split('#')[1];
-    let label = document.querySelector(`tag-label[data-tab="${hash}"]`);
-    let tab = document.querySelector(`tag-tab[data-tab="${hash}"]`);
-    let labels = label.parentNode.querySelectorAll('tag-label');
-    let tabs = label.parentNode.parentNode.parentNode.querySelectorAll(':scope > tag-tabs > tag-tab');
-    let tabsArray = Array.prototype.slice.call(tabs);
-    let index = tabsArray.indexOf(tab);
-
-    labels.forEach(sibling => sibling.classList.remove('is-active'));
-    tabs.forEach(tab => {
-        tab.classList.remove('is-active');
-        tab.style.left = `${-100 * index}%`;
-    });
-    label.classList.add('is-active');
-    tab.classList.add('is-active');
-
-    window.location.hash = label.dataset.tab;
+//init clipboards
+let clipboards = document.querySelectorAll('tag-code');
+let codes = document.querySelectorAll(`table[id='CODE-WRAP']`);
+if (clipboards.length > 0) {
+    initClipboard();
 }
-document.querySelectorAll('tag-labels').forEach(labelset => {
-    labelset.querySelectorAll('tag-label').forEach((label, i) => {
-        label.addEventListener('click', e => {
-            let labels = label.parentNode.querySelectorAll('tag-label');
-            let tabs = label.parentNode.parentNode.parentNode.querySelectorAll(':scope > tag-tabs > tag-tab');
-            labels.forEach(sibling => sibling.classList.remove('is-active'));
-            tabs.forEach(tab => {
-                tab.classList.remove('is-active');
-                tab.style.left = `${-100 * i}%`;
-            });
-            label.classList.add('is-active');
-            tabs[i].classList.add('is-active');
-    
-            if(label.dataset.tab) {
-                window.location.hash = label.dataset.tab;
-            }
-        });
-    });
-});
+if (codes.length > 0) {
+    initCodebox();
+}
+
+//init tabs
+let tabInstances = document.querySelectorAll('tag-labels');
+if(tabInstances.length > 0) {
+    initTabs();
+
+    //if hash
+    if(window.location.hash) {
+        initHashTabs();
+    }
+}
+
+//init profile
+if(pageID === 'Profile') {
+    clipWarnings();
+}
